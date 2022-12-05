132d4e9a892ad8c688085bbd02f4f591444d4505
JeongHyunAHN <jeonghyuna@me.com>
2022-12-05 17:22:01 +0100

    added 'function' block on Inventory (interface only)
    need to implement system

--- Assets/AutomaticScript/SendStatements_wrapper.cs
+++ Assets/AutomaticScript/SendStatements_wrapper.cs
@@ -28,4 +28,34 @@ public class SendStatements_wrapper : BaseWrapper
 		MainLoop.callAppropriateSystemMethod (system, "LevelCompleteStatement", null);
 	}
 
+	public void ActionDraggredStatement()
+	{
+		MainLoop.callAppropriateSystemMethod (system, "ActionDraggredStatement", null);
+	}
+
+	public void CharacterMovedStatement()
+	{
+		MainLoop.callAppropriateSystemMethod (system, "CharacterMovedStatement", null);
+	}
+
+	public void CollectedObjetStatement()
+	{
+		MainLoop.callAppropriateSystemMethod (system, "CollectedObjetStatement", null);
+	}
+
+	public void ActivatedDoorStatement()
+	{
+		MainLoop.callAppropriateSystemMethod (system, "ActivatedDoorStatement", null);
+	}
+
+	public void HackedRobotStatement()
+	{
+		MainLoop.callAppropriateSystemMethod (system, "HackedRobotStatement", null);
+	}
+
+	public void ReachedTPStatement(System.String time)
+	{
+		MainLoop.callAppropriateSystemMethod (system, "ReachedTPStatement", time);
+	}
+
 }
