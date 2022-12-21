c14cd2395420472f6703da2b04a4107626fbe0f7
KomeRice <golde150@gmail.com>
2022-12-22 00:15:28 +0100

    add script xml saving

--- Assets/AutomaticScript/SaveFileSystem_wrapper.cs
+++ Assets/AutomaticScript/SaveFileSystem_wrapper.cs
@@ -4,10 +4,12 @@ using FYFY;
 public class SaveFileSystem_wrapper : BaseWrapper
 {
 	public UnityEngine.UI.InputField executionLimitField;
+	public UnityEngine.GameObject editableContainer;
 	private void Start()
 	{
 		this.hideFlags = HideFlags.NotEditable;
 		MainLoop.initAppropriateSystemField (system, "executionLimitField", executionLimitField);
+		MainLoop.initAppropriateSystemField (system, "editableContainer", editableContainer);
 	}
 
 	public void saveXmlFile()
--- Assets/Systems/SaveFileSystem.cs
+++ Assets/Systems/SaveFileSystem.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml.Linq;
 using UnityEngine;
 using FYFY;
+using TMPro;
 using UnityEngine.UI;
-using ArgumentNullException = System.ArgumentNullException;
 
 public class SaveFileSystem : FSystem
 {
@@ -12,6 +14,7 @@ public class SaveFileSystem : FSystem
 	private Family f_levelData = FamilyManager.getFamily(new AllOfComponents(typeof(LevelData)));
 
 	public InputField executionLimitField;
+	public GameObject editableContainer;
 
 	public static SaveFileSystem instance;
 
@@ -76,6 +79,8 @@ public class SaveFileSystem : FSystem
 		}
 		rootNode.Add(xmlBlockLimits);
 
+		var robots = new Dictionary<string, Robot>();
+
 		foreach (var foCoords in getPaintableGrid().floorObjects.Keys)
 		{
 			var fo = getPaintableGrid().floorObjects[foCoords];
@@ -105,6 +110,7 @@ public class SaveFileSystem : FSystem
 							new XAttribute("posY", pr.y),
 							new XAttribute("posX", pr.x),
 							new XAttribute("direction", (int) pr.orientation)));
+					robots[pr.associatedScriptName] = pr;
 					break;
 				case EnemyRobot er:
 					rootNode.Add(
@@ -116,6 +122,7 @@ public class SaveFileSystem : FSystem
 							new XAttribute("range", er.range),
 							new XAttribute("selfRange", er.selfRange ? "True" : "False"),
 							new XAttribute("typeRange", (int) er.typeRange)));
+					robots[er.associatedScriptName] = er;
 					break;
 				case DecorationObject deco:
 					rootNode.Add(
@@ -145,8 +152,29 @@ public class SaveFileSystem : FSystem
 			rootNode.Add(new XElement("score", new XAttribute("twoStars", levelData.scoreTwoStars),
 				new XAttribute("threeStars", levelData.scoreThreeStars)));
 		}
-		
-		
+
+		for (var i = 0; i < editableContainer.transform.childCount; i++)
+		{
+			var editorViewportScriptContainer = editableContainer.transform.GetChild(i).Find("ScriptContainer");
+			var header = editorViewportScriptContainer.Find("Header");
+			var scriptName = header.Find("ContainerName").GetComponent<TMP_InputField>().text;
+			var scriptNode = (getXmlScript(editorViewportScriptContainer.gameObject, "script"));
+			if (!robots.ContainsKey(scriptName))
+			{
+				scriptNode.Add(new XAttribute("name", scriptName),
+					new XAttribute("editMode", 0),
+					new XAttribute("type", 3));
+				rootNode.Add(scriptNode);
+				continue;
+			}
+
+			var robot = robots[scriptName];
+			scriptNode.Add(new XAttribute("name", robot.associatedScriptName), 
+				new XAttribute("editMode", (int) robot.scriptEditMode),
+				new XAttribute("type", (int) robot.scriptType));
+			
+			rootNode.Add(scriptNode);
+		}
 		
 		xDocument.Add(rootNode);
 		Debug.Log($"Done: \n{xDocument}");
@@ -182,6 +210,200 @@ public class SaveFileSystem : FSystem
 		return result;
 	}
 
+	private XElement getXmlScript(GameObject cur, string name, XAttribute attribute = null)
+	{
+		var result = new XElement(name);
+		if(attribute != null)
+			result.Add(attribute);
+		
+		for (var i = 0; i < cur.transform.childCount; i++)
+		{
+			var child = cur.transform.GetChild(i).gameObject;
+			var actionComponent = child.GetComponent<BasicAction>();
+			if (actionComponent)
+			{
+				result.Add(getXmlAction(actionComponent.actionType));
+				continue;
+			}
+			
+			// IfElse inherits from IfControl so it must be checked first
+			
+			var ifElseComponent = child.GetComponent<IfElseControl>();
+			if (ifElseComponent)
+			{
+				
+				var conditionXml = new XElement("condition", getXmlCondition(child.transform.Find("ConditionContainer").GetChild(0).gameObject));
+				var thenContainer = child.transform.Find("Container");
+				var elseContainer = child.transform.Find("ElseContainer");
+				var thenXml = getXmlScript(thenContainer.gameObject, "thenContainer");
+				var elseXml = getXmlScript(elseContainer.gameObject, "elseContainer");
+				result.Add(new XElement("ifElse", conditionXml, thenXml, elseXml));
+				
+				continue;
+			}
+			
+			var ifThenComponent = child.GetComponent<IfControl>();
+			if (ifThenComponent)
+			{
+				var conditionXml = new XElement("condition", getXmlCondition(child.transform.Find("ConditionContainer").GetChild(0).gameObject));
+				var thenContainer = child.transform.Find("Container");
+				var thenXml = getXmlScript(thenContainer.gameObject, "container");
+				result.Add(new XElement("if", conditionXml, thenXml));
+				continue;
+			}
+			
+			
+			// WhileComponent inherits from ForControl so it must be checked before the latter
+			
+			var whileComponent = child.GetComponent<WhileControl>();
+			if (whileComponent)
+			{
+				var conditionXml = new XElement("condition", getXmlCondition(child.transform.Find("ConditionContainer").GetChild(0).gameObject));
+				var thenContainer = child.transform.Find("Container");
+				var thenXml = getXmlScript(thenContainer.gameObject, "container");
+				result.Add(new XElement("while", conditionXml, thenXml));
+				continue;
+			}
+			
+			var forComponent = child.GetComponent<ForControl>();
+			if (forComponent)
+			{
+				var nbForStr = forComponent.transform.Find("Header").GetComponentInChildren<TMP_InputField>().text;
+				var nbFor = string.IsNullOrEmpty(nbForStr) ? "0" : nbForStr;
+				result.Add(getXmlScript(child.transform.Find("Container").gameObject, "for", new XAttribute("nbFor", nbFor)));
+				continue;
+			}
+
+			var foreverComponent = child.GetComponent<ForeverControl>();
+			if (foreverComponent)
+			{
+				var thenContainer = child.transform.Find("Container");
+				var thenXml = getXmlScript(thenContainer.gameObject, "container");
+				result.Add(new XElement("forever", thenXml));
+			}
+		}
+
+		return result;
+	}
+
+	private XElement getXmlCondition(GameObject cur)
+	{
+		var captor = cur.GetComponent<BaseCaptor>();
+		string name;
+		if (captor)
+		{
+			name = captor.captorType switch
+			{
+				BaseCaptor.CaptorType.WallFront => "Wall",
+				BaseCaptor.CaptorType.WallLeft => "WallLeft",
+				BaseCaptor.CaptorType.WallRight => "WallRight",
+				BaseCaptor.CaptorType.Enemy => "Enemie",
+				BaseCaptor.CaptorType.RedArea => "RedArea",
+				BaseCaptor.CaptorType.FieldGate => "FieldGate",
+				BaseCaptor.CaptorType.Terminal => "Terminal",
+				BaseCaptor.CaptorType.Exit => "Exit",
+				_ => throw new ArgumentOutOfRangeException()
+			};
+			return new XElement("captor", new XAttribute("type", name));
+		}
+		
+
+		var operatorComponent = cur.GetComponent<BaseOperator>();
+		if (!operatorComponent)
+		{
+			Debug.Log("Invalid condition");
+			return new XElement("InvalidOperator");
+		}
+
+		name = operatorComponent.operatorType switch
+		{
+			BaseOperator.OperatorType.AndOperator => "and",
+			BaseOperator.OperatorType.OrOperator => "or",
+			BaseOperator.OperatorType.NotOperator => "not",
+			_ => throw new ArgumentOutOfRangeException()
+		};
+
+		var result = new XElement(name);
+
+		var container = cur.transform.Find("Container");
+		switch (operatorComponent.operatorType)
+		{
+			case BaseOperator.OperatorType.AndOperator:
+			case BaseOperator.OperatorType.OrOperator:
+				
+				var conditions = new List<GameObject>();
+				for (var i = 0; i < container.childCount; i++)
+				{
+					var child = container.GetChild(i);
+					if (child.GetComponent<BaseOperator>())
+					{
+						conditions.Add(child.gameObject);
+						continue;
+					}
+
+					if (child.GetComponent<BaseCondition>())
+					{
+						conditions.Add(child.gameObject);
+					}
+				}
+
+				if (conditions.Count != 2)
+				{
+					Debug.Log("Invalid condition");
+					return new XElement("InvalidCondition");
+				}
+				result.Add(new XElement("conditionLeft", getXmlCondition(conditions[0])));
+				result.Add(new XElement("conditionRight", getXmlCondition(conditions[1])));
+				
+				break;
+			case BaseOperator.OperatorType.NotOperator:
+				for (var i = 0; i < container.childCount; i++)
+				{
+					var child = container.GetChild(i);
+					if (child.GetComponent<BaseOperator>())
+					{
+						result.Add(getXmlCondition(child.gameObject));
+						break;
+					}
+
+					if (!child.GetComponent<BaseCondition>()) 
+						continue;
+					
+					result.Add(getXmlCondition(child.gameObject));
+					break;
+				}
+
+				if (result.Elements().Count() != 1)
+				{
+					Debug.Log("Invalid condition");
+					return new XElement("InvalidCondition");
+				}
+				
+				break;
+			default:
+				Debug.Log("Invalid condition");
+				return new XElement("InvalidCondition");
+		}
+
+		return result;
+	}
+
+	private XElement getXmlAction(BasicAction.ActionType type)
+	{
+		var actionString = type switch
+		{
+			BasicAction.ActionType.Activate => "Activate",
+			BasicAction.ActionType.Forward => "Forward",
+			BasicAction.ActionType.TurnLeft => "TurnLeft",
+			BasicAction.ActionType.TurnRight => "TurnRight",
+			BasicAction.ActionType.Wait => "Wait",
+			BasicAction.ActionType.TurnBack => "TurnBack",
+			_ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+		};
+
+		return new XElement("action", new XAttribute("type", actionString));
+	}
+	
 	private PaintableGrid getPaintableGrid()
 	{
 		return f_paintables.First().GetComponent<PaintableGrid>();
